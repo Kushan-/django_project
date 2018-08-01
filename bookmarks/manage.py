5d4f98e29116f99842596b585aec78e06e65a589
kush <kush@josephnogucci.com>
2018-08-01 11:16:32 -0400

    comencing new django project

--- bookmarks/manage.py
+++ bookmarks/manage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-#run commands
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartAppDemo.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmarks.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
